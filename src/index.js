246c55e2480d190998909ea1f0d966261585eba5
Vincent Clemson <vclemson07@gmail.com>
2020-03-30 02:08:22 -0400

    create bundle by running webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
