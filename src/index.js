69077c4e0fc5e74fadc0bd77d033578b4a97614c
Vincent Clemson <vclemson07@gmail.com>
2020-04-12 20:14:55 -0400

    add css loader to bundle css asset, change main.js to bundle.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
