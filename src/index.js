ec79e0a92611ea12657ef329f2415230f3fafa4b
Vincent Clemson <vclemson07@gmail.com>
2020-04-26 02:57:20 -0400

    Incoporate Hot Module Replacement into app through webpack and webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import printMe from './print.js'
+import './style.css'
 
 function component () {
   const element = document.createElement('div')
@@ -15,4 +16,15 @@ function component () {
   return element
 }
 
-document.body.appendChild(component())
+// Store the element to re-render on print.js changes
+const element = component()
+document.body.appendChild(element)
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting thee updated printMe module!')
+    document.body.removeChild(element)
+    element = component() // Re-render the "component" to update the click handler
+    document.body.appendChild(element)
+  })
+}
--- src/print.js
+++ src/print.js
@@ -1,7 +1,5 @@
 export default function printMe () {
   console.log(
-    `My index.html gets deleted when using the clean-webpack-plugin.
-    \nI get called from print.js!
-    \nIt's alive!`
+    'Updating print.js ya man'
   )
 }
